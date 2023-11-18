bfa9f965c9e51b9f91ca960d30d9e6b6a80597ad
tfer2442 <tfer2442@gmail.com>
2023-11-19 01:30:32 +0900

    refactor(MovieService): 인스턴스 변수로 선언한 리스트들을 필드 변수로 변경

--- src/main/java/com/ktrendiest/newton/service/MovieService.java
+++ src/main/java/com/ktrendiest/newton/service/MovieService.java
@@ -22,11 +22,6 @@ import com.ktrendiest.newton.constant.UrlConstant;
 public class MovieService {
     private final String koficKey;
     private final String kmdbKey;
-    private final List<String> titles = new ArrayList<>();
-    private final List<String> ranks = new ArrayList<>();
-    private final List<String> openDates = new ArrayList<>();
-    private final List<String> imageLinks = new ArrayList<>();
-    private final List<String> infoLinks = new ArrayList<>();
     private List<Movie> movieInfos;
 
     public MovieService(@Value("${kofic-key}") String koficKey,
@@ -42,30 +37,36 @@ public class MovieService {
     @PostConstruct
     @Scheduled(cron = "0 0 1 * * *")
     private void initialize() {
-        fetchDataFromKofic();
-        fetchDataFromKmdb();
-        createMovies();
+        List<String> ranks = new ArrayList<>();
+        List<String> titles = new ArrayList<>();
+        List<String> openDates = new ArrayList<>();
+        List<String> imageLinks = new ArrayList<>();
+        List<String> infoLinks = new ArrayList<>();
+
+        String koficData = getDataFromKofic();
+        addRankAndName(koficData, ranks, titles, openDates);
+        fetchDataFromKmdb(titles, openDates, imageLinks, infoLinks);
+        createMovies(ranks, titles, imageLinks, infoLinks);
     }
 
-    private void fetchDataFromKofic() {
+    private String getDataFromKofic() {
         String targetDate = getTargetDate();
         String baseUrl = UrlConstant.KOFIC_BASE_URL;
         String koficUrl = getKoficDynamicUrl(baseUrl, targetDate);
-        String koficData = getKoficResponseBody(koficUrl);
-        addRankAndName(koficData);
+        return getKoficResponseBody(koficUrl);
     }
 
-    private void fetchDataFromKmdb() {
+    private void fetchDataFromKmdb(List<String> titles, List<String> openDates, List<String> imageLinks, List<String> infoLinks) {
         String baseUrl = UrlConstant.KMDB_BASE_URL;
 
-        for (int i = 0; i < 10; i++) {
-            String dynamicUrl = getKmdbDynamicUrl(baseUrl, i);
+        for (int i = 0; i < TOTAL_ITEMS_NUMBER; i++) {
+            String dynamicUrl = getKmdbDynamicUrl(baseUrl, titles, openDates, i);
             String responseBody = getKmdbResponseBody(dynamicUrl);
 
             ObjectMapper objectMapper = new ObjectMapper();
             try {
                 JsonNode rootNode = objectMapper.readTree(responseBody);
-                addKmdbUrlAndPosters(rootNode);
+                addKmdbUrlAndPosters(rootNode, imageLinks, infoLinks);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -98,7 +99,7 @@ public class MovieService {
                 .block();
     }
 
-    private void addRankAndName(String jsonData) {
+    private void addRankAndName(String jsonData, List<String> ranks, List<String> titles, List<String> openDates) {
         ObjectMapper objectMapper = new ObjectMapper();
         DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
 
@@ -118,7 +119,7 @@ public class MovieService {
         }
     }
 
-    private String getKmdbDynamicUrl(String baseUrl, int num) {
+    private String getKmdbDynamicUrl(String baseUrl, List<String> titles, List<String> openDates, int num) {
         return UriComponentsBuilder.fromHttpUrl(baseUrl)
                 .queryParam("ServiceKey", kmdbKey)
                 .queryParam("title", titles.get(num))
@@ -137,7 +138,7 @@ public class MovieService {
     }
 
 
-    private void addKmdbUrlAndPosters(JsonNode rootNode) {
+    private void addKmdbUrlAndPosters(JsonNode rootNode, List<String> imageLinks, List<String> infoLinks) {
         JsonNode resultNode = rootNode
                 .path("Data")
                 .path(0)
@@ -154,7 +155,7 @@ public class MovieService {
         infoLinks.add(kmdbUrlNode.asText());
     }
 
-    private void createMovies() {
+    private void createMovies(List<String> ranks, List<String> titles, List<String> imageLinks, List<String> infoLinks) {
         List<Movie> movies = new ArrayList<>();
 
         for (int i = 0; i < TOTAL_ITEMS_NUMBER; i++) {
